2bddf9da3374a46455fa4a04903a5a69a9ae98b3
astar092 <astar.bekturov@gmail.com>
2023-01-29 23:11:13 +0800

    Authentification

---
+++ lang/ru/passwords.php
@@ -0,0 +1,22 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Password Reset Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines are the default lines which match reasons
+    | that are given by the password broker for a password update attempt
+    | has failed, such as for an invalid token or invalid new password.
+    |
+    */
+
+    'reset' => 'Ваш пароль был изменен!',
+    'sent' => 'Мы отправили вам ссылку для сброса пароля!',
+    'throttled' => 'Пожалуйста подождите.',
+    'token' => 'Этот токен уже не действителен.',
+    'user' => "Мы не можем найти пользователя с таким емайлом.",
+
+];
